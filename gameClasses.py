679928688906bad84c6adfe2f8c3d1ec70276733
Gleb <GlebMamedov@icloud.com>
2020-12-01 23:08:54 +0300

    COM-0002: save some code
    Issues: reduce the number of useless vars and lines
    
    Fix: bouncing on rects
    
    TODO: Health, bulets, serverpart

--- gameClasses.py
+++ gameClasses.py
@@ -17,29 +17,31 @@ class GameObject:
     def __init__(self, width, height, x, y):
         self.width = width
         self.height = height
-        self.shape = pygame.Rect(x, y, width, height)
         self.x = x
         self.y = y
+        self.shape = pygame.Rect(x, y, width, height)
+        self.top = self.shape.top
+        self.bottom = self.shape.bottom
 
     def obj_draw(self):
         pass
 
     def obj_move(self, newX, newY):
-        if __name__ == '__main__':
-            self.shape = self.shape.move(newX, newY)
+        self.shape = self.shape.move(newX, newY)
+
+    def obj_step(self, step, num_mas, x):
+        if num_mas[pygame.K_RIGHT]:
+            x += step
+        if num_mas[pygame.K_LEFT]:
+            x -= step
+        # self.shape = pygame.Rect(x, self.y, self.width, self.height)
+        return x
 
 
 class Player(GameObject):
     def __init__(self, step):
         self.step = step
 
-    def player_move(self, num_mas, step):
-        if num_mas[pygame.K_UP]:
-            y -= step
-        if num_mas[pygame.K_DOWN]:
-            y += step
-        if num_mas[pygame.K_RIGHT]:
-            x += step
-        if num_mas[pygame.K_LEFT]:
-            x -= step
-        return x, y
+# edit jump
+    def player_jump(self):
+        self.y = y**2 - 12
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import gameClasses
+import sys
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
@@ -9,29 +11,50 @@ bottomBorder = gameClasses.GameObject(600, 10, 0, 490)
 rightBorder = gameClasses.GameObject(10, 480, 0, 10)
 leftBorder = gameClasses.GameObject(10, 480, 590, 10)
 
+brick = gameClasses.GameObject(80, 10, 30, 370)
+
 borders = [topBorder.shape, bottomBorder.shape, rightBorder.shape, leftBorder.shape]
-playerX = 40
-playerY = 40
+playerX = 10
+playerY = 430
+jumpH = 8
+jumpSt = False
+falling = False
 
 step = 10
 gameRun = True
 
+player = gameClasses.GameObject(60, 60, playerX, playerY)
 
-def player_enter(num_mas, step, x, y):
-    if num_mas[pygame.K_UP]:
-        y -= step
-    if num_mas[pygame.K_DOWN]:
-        y += step
+
+# edit func with consequences
+def player_enter(num_mas, step, x, status):
     if num_mas[pygame.K_RIGHT]:
         x += step
     if num_mas[pygame.K_LEFT]:
         x -= step
-    return x, y
+    if keys[pygame.K_UP]:
+        status = True
+    return x, status
+
+
+def player_jump(y, high, x, jumpstat, fall):
+    x -= 1
+    if abs(x) <= 8:
+        if x > 0:
+            y -= (-(x**2) + high)//2
+        if x < 0:
+            y += (-(x**2) + high)//2
+            fall = True
+        return y, x, jumpstat, fall
+    else:
+        fall = False
+        jumpstatus = False
+        return y, x, jumpstatus, fall
 
 
 while gameRun:
-    pygame.time.delay(50)
-    playerPos = (playerX, playerY)
+    pygame.time.delay(40)
+    playerPos = (player.x, player.y)
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,14 +62,27 @@ while gameRun:
         if keys[pygame.K_ESCAPE]:
             gameRun = False
 
-    playerX, playerY = player_enter(keys, step, playerX, playerY)
-    for border in borders:
-        testRect = pygame.Rect(playerX, playerY, 60, 60)
-        if testRect.colliderect(border):
-            playerX, playerY = playerPos
+    player.x, jumpSt = player_enter(keys, step, player.x, jumpSt)
 
+    if jumpSt:
+        player.y, jumpH, jumpSt, falling = player_jump(player.y, 64, jumpH, jumpSt, falling)
+    else:
+        jumpH = 10
+    print(falling)
+    for border in borders:
+        testRect = pygame.Rect(player.x, player.y, 60, 60)
+        if testRect.colliderect(border):
+            if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+                player.x = playerPos[0]
+    if player.shape.colliderect(brick.shape):
+        if falling:
+            jumpSt = False
+            player.y = brick.height + 300
+    print(player.y)
+    player.shape = pygame.Rect(player.x, player.y, 60, 60)
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (40, 240, 250), (playerX, playerY, 60, 60 ))
+    pygame.draw.rect(screen, (40, 240, 250), player.shape)
+    pygame.draw.rect(screen, (250, 250, 0), brick.shape)
     pygame.draw.rect(screen, (20, 100, 50), topBorder.shape)
     pygame.draw.rect(screen, (20, 100, 20), bottomBorder.shape)
     pygame.draw.rect(screen, (50, 130, 60), rightBorder.shape)
